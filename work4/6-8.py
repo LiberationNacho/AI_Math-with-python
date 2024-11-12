e70d97a8c85ec57102925262e17088776640785b
henac <man25816@naver.com>
2024-11-13 00:24:55 +0900

    .

--- work4/6-8.py
+++ work4/6-8.py
@@ -4,10 +4,31 @@ import numpy as np
 L_a = np.array([[3, 0], [0, 4]])
 print("(a) 표준행렬:\n", L_a)
 
+# 특잇값 분해
+U_a, S_a, Vt_a = np.linalg.svd(L_a)
+print("(a) 특잇값 분해 결과:")
+print("U_a:\n", U_a)
+print("S_a:\n", S_a)
+print("Vt_a:\n", Vt_a)
+
 # (b) L(x, y) = (x + y, y, x)
 L_b = np.array([[1, 1], [0, 1], [1, 0]])
-print("(b) 표준행렬:\n", L_b)
+print("\n(b) 표준행렬:\n", L_b)
+
+# 특잇값 분해
+U_b, S_b, Vt_b = np.linalg.svd(L_b)
+print("(b) 특잇값 분해 결과:")
+print("U_b:\n", U_b)
+print("S_b:\n", S_b)
+print("Vt_b:\n", Vt_b)
 
 # (c) L(x, y, z) = (x + z, y)
 L_c = np.array([[1, 0, 1], [0, 1, 0]])
-print("(c) 표준행렬:\n", L_c)
+print("\n(c) 표준행렬:\n", L_c)
+
+# 특잇값 분해
+U_c, S_c, Vt_c = np.linalg.svd(L_c)
+print("(c) 특잇값 분해 결과:")
+print("U_c:\n", U_c)
+print("S_c:\n", S_c)
+print("Vt_c:\n", Vt_c)
--- work4/6-9.py
+++ work4/6-9.py
@@ -15,6 +15,20 @@ A_a_AT = A_a @ A_a.T
 eigenvalues_A_a_TA = np.linalg.eigvals(A_a_TA)
 eigenvalues_A_a_AT = np.linalg.eigvals(A_a_AT)
 
+# (a) A^T A와 AA^T의 특잇값 분해
+U_a_TA, S_a_TA, Vt_a_TA = np.linalg.svd(A_a_TA)
+U_a_AT, S_a_AT, Vt_a_AT = np.linalg.svd(A_a_AT)
+
+print("(a) A^T A의 특잇값 분해:")
+print("U_a_TA:\n", U_a_TA)
+print("S_a_TA (특잇값):", S_a_TA)
+print("Vt_a_TA:\n", Vt_a_TA)
+
+print("(a) AA^T의 특잇값 분해:")
+print("U_a_AT:\n", U_a_AT)
+print("S_a_AT (특잇값):", S_a_AT)
+print("Vt_a_AT:\n", Vt_a_AT)
+
 print("(a) A^T A:\n", A_a_TA)
 print("(a) A^T A의 고유값:", eigenvalues_A_a_TA)
 print("(a) AA^T:\n", A_a_AT)
@@ -33,6 +47,20 @@ A_b_AT = A_b @ A_b.T
 # (b) 각 행렬의 고유값 계산
 eigenvalues_A_b_TA = np.linalg.eigvals(A_b_TA)
 eigenvalues_A_b_AT = np.linalg.eigvals(A_b_AT)
+
+# (b) A^T A와 AA^T의 특잇값 분해
+U_b_TA, S_b_TA, Vt_b_TA = np.linalg.svd(A_b_TA)
+U_b_AT, S_b_AT, Vt_b_AT = np.linalg.svd(A_b_AT)
+
+print("\n(b) A^T A의 특잇값 분해:")
+print("U_b_TA:\n", U_b_TA)
+print("S_b_TA (특잇값):", S_b_TA)
+print("Vt_b_TA:\n", Vt_b_TA)
+
+print("(b) AA^T의 특잇값 분해:")
+print("U_b_AT:\n", U_b_AT)
+print("S_b_AT (특잇값):", S_b_AT)
+print("Vt_b_AT:\n", Vt_b_AT)
 
 print("\n(b) A^T A:\n", A_b_TA)
 print("(b) A^T A의 고유값:", eigenvalues_A_b_TA)
